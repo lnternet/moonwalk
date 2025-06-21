afc0990028671660ee9d29a215fea6fdd8ca5d2a
Jaroslav <jaroslav-vercinskij@outlook.com>
2025-06-21 23:49:37 +0300

    Initial

---
+++ app/components/dog.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import "./dog.css";
+
+export const Dog: React.FC = () => {
+  return (
+    <>
+      <div className="container">
+        <div id="dog_belly"></div>
+        <div id="helmet"></div>
+        <div id="dog_head">
+          <div id="dog_eye">
+            <div id="dog_eye_pupil"></div>
+          </div>
+          <div id="dog_nose"></div>
+          <div id="dog_nose_spot"></div>
+          <div id="dog_ear_right"></div>
+        </div>
+        <div id="dog_leg_frontright"></div>
+        <div id="dog_leg_backright"></div>
+        <div id="dog_leg_backleft"></div>
+        <div id="dog_leg_frontleft"></div>
+        <div id="tail"></div>
+      </div>
+      <div id="shadow"></div>
+    </>
+  );
+};
