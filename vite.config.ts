f8652f91ac45e5c63ccdee3d6b42fc5a72a603cf
Jaroslav <jaroslav-vercinskij@outlook.com>
2025-06-22 23:20:17 +0300

    Fix23

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,6 @@ import tsconfigPaths from "vite-tsconfig-paths";
 export default defineConfig({
   plugins: [reactRouter(), tsconfigPaths()],
   build: {
-    cssCodeSplit: false,
+    modulePreload: false,
   },
 });
